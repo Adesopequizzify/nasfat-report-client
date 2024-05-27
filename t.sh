f7cc4c4406b707e22f314630a9147748e7dfeadf
Adesopequizzify <adesope@quizzify.com.ng>
2024-05-27 19:00:26 +0100

    we added a feautre

---
+++ t.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "HTML files:"
+find public -type f -name "*.html"
+
+echo "\nCSS files:"
+find public -type f -name "*.css"
+
+echo "\nJavaScript files:"
+find public -type f -name "*.js"
